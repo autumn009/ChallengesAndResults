439670cd8772974a7051b9bdd33f43db5657127b
Akira Kawamata <autumn@piedey.co.jp>
2019-01-18 15:37:53 +0900

    temp save

--- ChallengesAndResults/ChallengesAndResults/Util.cs
+++ ChallengesAndResults/ChallengesAndResults/Util.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ChallengesAndResults
 {
     public class Util
     {
-        public static void SetResource(string name)
+        public static List<string> Names;
+        public static string ManifestName;
+
+        public static void SetResource(string name, string cname)
         {
-
+            ManifestName = cname;
+            Names = new List<string>();
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream("ChallengesAndResults." + name))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    for (; ; )
+                    {
+                        var s = reader.ReadLine();
+                        if (s == null) return;
+                        if (s.StartsWith("#")) continue;
+                        Names.Add(s);
+                    }
+                }
+            }
         }
-
-
     }
 }
