be743abf155509556018114ac4960ba9c3ba0466
Akira Kawamata <autumn@piedey.co.jp>
2019-01-18 14:56:22 +0900

    temp save

---
+++ ChallengesAndResults/ChallengesAndResults/Util.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengesAndResults
+{
+    public class Util
+    {
+        public static void SetResource(string name)
+        {
+
+        }
+
+
+    }
+}
