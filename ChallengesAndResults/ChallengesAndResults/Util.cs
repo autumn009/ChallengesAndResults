cd4fe43794c680c6719888b17615389f2df89fa1
Akira Kawamata <autumn@piedey.co.jp>
2019-01-24 18:54:39 +0900

    change save format

--- ChallengesAndResults/ChallengesAndResults/Util.cs
+++ ChallengesAndResults/ChallengesAndResults/Util.cs
@@ -58,29 +58,34 @@ namespace ChallengesAndResults
             uriHelper = uriHelper0;
             //PageRefer = pageRefer0;
         }
+        class SerializeContainerItem
+        {
+            public string name;
+            public bool tryCheck;
+            public bool correctCheck;
+        }
+        class SerializeContainer
+        {
+            public SerializeContainerItem[] items;
+        }
 
-        private static async Task LoadBoolArray(List<bool> target, string name)
-        {
-            var r = await localStorage.GetItem<bool[]>(name + "_" + ManifestName);
-            if (r != null)
-            {
-                for (int i = 0; i < Math.Min(r.Length, Names.Count); i++)
-                {
-                    target[i] = r[i];
-                }
-            }
-        }
-        private static async Task SaveBoolArray(List<bool> target, string name)
-        {
-            if (target == null) return;
-            await localStorage.SetItem<bool[]>(name + "_" + ManifestName, target.ToArray());
-        }
         private static async Task Load()
         {
             //Console.WriteLine("Load Callded");
             clear();
-            await LoadBoolArray(TryChecks, "try");
-            await LoadBoolArray(CorrectChecks, "correct");
+            var r = await localStorage.GetItem<SerializeContainer>("v2_" + ManifestName);
+            parseContainer(r);
+        }
+
+        private static void parseContainer(SerializeContainer r)
+        {
+            for (int i = 0; i < Names.Count; i++)
+            {
+                var item = r.items.FirstOrDefault(c => c.name == Names[i]);
+                if (item == null) continue;
+                TryChecks[i] = item.tryCheck;
+                CorrectChecks[i] = item.correctCheck;
+            }
         }
 
         public static void clear()
@@ -92,14 +97,25 @@ namespace ChallengesAndResults
         public static async Task Save()
         {
             //Console.WriteLine("Save Callded");
-            await SaveBoolArray(TryChecks, "try");
-            await SaveBoolArray(CorrectChecks, "correct");
+            SerializeContainer r = createContainer();
+            await localStorage.SetItem<SerializeContainer>("v2_" + ManifestName, r);
         }
-        class SerializeContainer
+
+        private static SerializeContainer createContainer()
         {
-            public bool[] tryChecks;
-            public bool[] correctChecks;
+            var r = new SerializeContainer();
+            r.items = new SerializeContainerItem[Names.Count];
+            for (int i = 0; i < Names.Count; i++)
+            {
+                r.items[i] = new SerializeContainerItem();
+                r.items[i].name = Names[i];
+                r.items[i].tryCheck = TryChecks[i];
+                r.items[i].correctCheck = CorrectChecks[i];
+            }
+
+            return r;
         }
+
         // this is workaround for Blazor 0.70
         // https://github.com/mono/mono/issues/11848
         public static void Workaround()
@@ -111,9 +127,7 @@ namespace ChallengesAndResults
         public static string Export()
         {
             Workaround();
-            var container = new SerializeContainer();
-            container.tryChecks = TryChecks.ToArray();
-            container.correctChecks = CorrectChecks.ToArray();
+            var container = createContainer();
             var json = JsonConvert.SerializeObject(container);
             var bytear = Encoding.UTF8.GetBytes(json);
             return "data:application/octet-stream;base64," + Convert.ToBase64String(bytear);
@@ -128,17 +142,7 @@ namespace ChallengesAndResults
             var s = Encoding.UTF8.GetString(bytes);
             //Console.WriteLine(s);
             var deserialized = JsonConvert.DeserializeObject<SerializeContainer>(s);
-            //Console.WriteLine(deserialized.tryChecks.Count());
-            for (int i = 0; i < Math.Min(Names.Count(), deserialized.tryChecks.Count()); i++)
-            {
-                //Console.WriteLine(deserialized.tryChecks[i]);
-                TryChecks[i] = deserialized.tryChecks[i];
-            }
-            for (int i = 0; i < Math.Min(Names.Count(), deserialized.correctChecks.Count()); i++)
-            {
-                //Console.WriteLine(deserialized.correctChecks[i]);
-                CorrectChecks[i] = deserialized.correctChecks[i];
-            }
+            parseContainer(deserialized);
             PageRefer.StateHasChangedProxy();
             _ = Util.Save();
         }
